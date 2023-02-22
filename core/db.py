6ae30f8a3939c79162addecfd5792de684bda28e
Brad Figg <bfigg@nvidia.com>
2023-02-22 10:50:28 -0800

    load-bugs, core/db.py: expand the number of fields that we are storing for each bug into the database tables
    Signed-off-by: Brad Figg <bfigg@nvidia.com>

--- core/db.py
+++ core/db.py
@@ -1,5 +1,4 @@
 from bcore.std                          import pre
-from nvcore.nvcreds                     import nvkpis_postgres
 from datetime                           import datetime
 import psycopg2
 from os                                 import path
@@ -10,9 +9,18 @@ def pg_config():
     with open(path.expanduser('~/.pg_postgres'), 'r') as fd:
         return json.loads(fd.read())
 
+def quote(string):
+    return f"'{string}'"
+
+def ts(string):
+    try:
+        return quote(datetime.strptime(string, '%m/%d/%Y %H:%M:%S %p'))
+    except ValueError:
+        return 'NULL'
+
 class DBBase():
     def __init__(self):
-        pgcfg = nvkpis_postgres()
+        pgcfg = pg_config()
         self.pgcon = None
         self.pgcon = psycopg2.connect(host=pgcfg['host'], port=pgcfg['port'], database=pgcfg['database'], user=pgcfg['user'], password=pgcfg['password'])
 
@@ -33,25 +41,49 @@ class DBBase():
         q  = f'drop table {self._table_name};'
         self.execute(q)
 
-class BugTable(DBBase):
+class BugsTable(DBBase):
     def __init__(self, module):
-        super(BugTable, self).__init__()
-        self._table_name = f"{module.replace(' ', '_')}_bugs"
+        super(BugsTable, self).__init__()
+        self._table_name = f"{module.lower().replace('-', '').replace(' ', '_')}_bugs"
 
     def create_schema(self):
         q  = 'create table if not exists '
         q += f'{self._table_name} ( '
-        q += '    id integer,'
+        q += '    id integer primary key,'
         q += '    module varchar,'
+        q += '    regression varchar,'
+        q += '    disposition varchar,'
+        q += '    ts_opened timestamp,'
+        q += '    ts_closed timestamp,'
+        q += '    ts_fixed timestamp,'
+        q += '    os varchar,'
+        q += '    requester varchar,'
+        q += '    engineer varchar'
         q += ');'
         self.execute(q)
 
     def add(self, m):
         q  = f'INSERT INTO {self._table_name} ('
         q += 'id, '
-        q += 'module, '
+        q += 'module,'
+        q += 'regression,'
+        q += 'disposition,'
+        q += 'ts_opened,'
+        q += 'ts_closed,'
+        q += 'ts_fixed,'
+        q += 'os,'
+        q += 'requester,'
+        q += 'engineer'
         q += ') VALUES ('
         q += f'{m["id"]},'
-        q += f'{m["module"]}'
+        q += f'{quote(m["module"])},'
+        q += f'{quote(m["regression"])},'
+        q += f'{quote(m["disposition"])},'
+        q += f'{ts(m["ts_opened"])},'
+        q += f'{ts(m["ts_closed"])},'
+        q += f'{ts(m["ts_fixed"])},'
+        q += f'{quote(m["os"])},'
+        q += f'{quote(m["requester"])},'
+        q += f'{quote(m["engineer"])}'
         q += ');'
         self.execute(q)
