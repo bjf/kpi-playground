48721740af0382983d7f8f486c520403d380a7c2
Brad Figg <bfigg@nvidia.com>
2023-02-21 18:30:35 -0800

    database changed from 'kpis' to 'pg'
    Signed-off-by: Brad Figg <bfigg@nvidia.com>

--- db.pg.init.sh
+++ db.pg.init.sh
@@ -1,7 +1,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "kpis" --dbname "kpis" <<-EOSQL
-    CREATE DATABASE kpis;
+    CREATE DATABASE pg;
     CREATE USER kpis with ENCRYPTED PASSWORD 'kpis';
     GRANT ALL PRIVILEGES ON DATABASE kpis TO kpis;
 EOSQL
